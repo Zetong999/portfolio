362d3616d53de1ca8e422fdb548dd678acea9109
xianweilu <xianweilu@xianweilus-MBP.attlocal.net>
2025-07-31 17:41:26 -0700

    integrate-next-intl

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,17 +1,20 @@
 import Home from '@/components/home';
 import { getBlogForHome } from '@/graphqlApi';
 import { FaGithub, FaLinkedin } from 'react-icons/fa';
+import {getTranslations} from 'next-intl/server';
 
 export default async function HomePage() {
   const PostsData = await getBlogForHome()
 
+   const t = await getTranslations('HomePage');
+
 
   return (
     <div className="p-10 max-w-4xl mx-auto">
-      <h1 className="text-3xl font-bold mb-8">Welcome to My Portfolio</h1>
+      <h1 className="text-3xl font-bold mb-8">{t('title')}</h1>
 
       <p className="mb-6 italic">
-        AI, Data and Open Source is the future, and I am here to share my journey with you.
+        {t('about')}
       </p>
 
       <div className="space-x-4">
@@ -21,7 +24,7 @@ export default async function HomePage() {
           rel="noopener noreferrer"
           className="inline-flex items-center px-4 py-2 border border-black font-bold hover:bg-gray-100 rounded-none"
         >
-          GitHub
+          {t('buttons.github')}
           <FaGithub className="ml-2" /> {/* logo 在文字后面 */}
         </a>
 
@@ -31,7 +34,7 @@ export default async function HomePage() {
           rel="noopener noreferrer"
           className="inline-flex items-center px-4 py-2 border border-black font-bold hover:bg-gray-100 rounded-none"
         >
-          LinkedIn
+            {t('buttons.linkedIn')}
           <FaLinkedin className="ml-2" /> {/* logo 在文字后面 */}
         </a>
       </div>
